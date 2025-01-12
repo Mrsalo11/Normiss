d322b3192b4b9d8a2c84b7329183c9960d8fa325
Mrsalo11 <yarmolenkoevgenij13@gmail.com>
2025-01-12 14:45:33 +0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,67 @@
+
+body {
+    font-family: sans-serif;
+      background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT61WoWnZrDCpZTIM63eaNSBdyQbukSNE_LNQ&s");
+}
+header {
+    background: linear-gradient(to right, #ff7e5f, #feb47b);
+    border-radius: 15px;
+}
+main {
+     background: linear-gradient(to bottom, #ff7e5f, brown);
+     border-radius: 15px;
+}
+
+footer {
+    background-color: brown;
+    
+border-radius: 15px;
+}
+h1 {
+    text-align: center;
+}
+h2 {
+       text-shadow: 2px 2px 0 white;
+    text-align: center;
+     word-spacing: 15px;
+}
+ .sigma {
+border-radius: 20px;
+height: 90px;
+width: 100px;
+ transition: height 0.4s ease, width 1s ease;
+
+ }
+ .sigma:hover {
+    height: 140px;
+    width: 165px;
+    
+ }
+ h3 {
+    text-align: center;
+}
+
+p {
+  text-align: center;  
+   
+}
+ 
+.rectangle {
+    border: 2px solid black;
+    border-radius: 20px;
+    background-color: #ff7e5f;
+    transition: transform 0.6s ease-out;
+}
+ 
+ .rectangle:hover {
+ transform: rotate(2deg) scale(1.05);
+ } 
+ .rectangla {
+    border: 2px solid black;
+    border-radius: 20px;
+    background-color: rgb(169, 56, 56);
+    transition: transform 0.4s ease-out;
+}
+.rectangla:hover {
+ transform: rotate(-3deg) scale(1.04);
+ } 
